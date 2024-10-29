2b929e58cf55dbf1ec70f5318c64b2b0630591d1
madler202 <131901759+madler202@users.noreply.github.com>
2024-10-29 11:30:45 -0400

    Update AbstractUser.java

--- WLVP/src/wlvp/AccountCreation/AbstractUser.java
+++ WLVP/src/wlvp/AccountCreation/AbstractUser.java
@@ -7,12 +7,49 @@ import wlvp.IncidentReporting.ReportType;
  * @author Grant
  */
 public abstract class AbstractUser {
+   String username;
+   
+   String email;
+
+   String password;
+    
     /**
      * Unique integer identity
      */
     protected int ID;
     
+    public int getID(){ 
+        return ID;
+    }
     
+    public void setID(int ID){ 
+        this.ID = ID;        
+    }
+    
+    // Getters and Setters for username, email, and password
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     /**
      * Creates a new report
      * @param type The ReportType of the report to be created
@@ -20,5 +57,15 @@ public abstract class AbstractUser {
      */
     protected ReportType createReport(ReportType type) {
         return null;
+    }  
+    
+    @Override
+    public String toString() {
+        return "User{" +
+                "ID=" + ID +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
+
