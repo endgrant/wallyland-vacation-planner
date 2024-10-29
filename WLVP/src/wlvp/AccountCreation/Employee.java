5d94580536661a9cac9bd23a6b9cc738c03c21f7
madler202 <131901759+madler202@users.noreply.github.com>
2024-10-29 11:31:15 -0400

    Update Employee.java

--- WLVP/src/wlvp/AccountCreation/Employee.java
+++ WLVP/src/wlvp/AccountCreation/Employee.java
@@ -1,5 +1,10 @@
 package wlvp.AccountCreation;
 
+import java.util.List;
+import wlvp.Attraction.AbstractAttraction;
+import wlvp.IncidentReporting.Report;
+import static wlvp.IncidentReporting.Report.getAllReports;
+
 /**
  * Park employee class
  * @author Grant
@@ -13,9 +18,27 @@ public class Employee extends AbstractUser {
     
     /**
      * Default constructor
+     * @param id
+     * @param username
+     * @param email
+     * @param password
      * @param type The EmployeeType of this employee
      */
-    public Employee(EmployeeType type) {
+    public Employee(int id, String username, String email, String password, EmployeeType type) {
+        this.ID = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
         this.type = type;
     }
-}
+    
+  
+     public List <Report> viewReports() {
+         return getAllReports(); 
+    }
+
+    public void updateAttractions(AbstractAttraction attraction, boolean newStatus) {
+        attraction.setStatus(newStatus);
+       }
+    }
+
