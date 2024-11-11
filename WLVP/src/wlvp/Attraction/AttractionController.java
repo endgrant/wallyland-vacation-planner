4f346c41dd51ec4412d087aa0e45b004f60302b3
dhost44 <dvh5540@psu.edu>
2024-11-11 11:06:50 -0500

    Create AttractionController.java

---
+++ WLVP/src/wlvp/Attraction/AttractionController.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package wlvp.Attraction;
+
+/**
+ *
+ * @author Daniel
+ */
+public class AttractionController {
+    public static AttractionView attractionView;
+    public static AbstractAttraction attraction;
+    
+    
+          
+}
