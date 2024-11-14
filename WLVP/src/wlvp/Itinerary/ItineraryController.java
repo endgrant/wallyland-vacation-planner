13b002be226755d77a18faf6735c39ec6138349f
madler202 <131901759+madler202@users.noreply.github.com>
2024-11-14 13:35:08 -0500

    Fixed ItineraryView

---
+++ WLVP/src/wlvp/Itinerary/ItineraryController.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package wlvp.Itinerary;
+
+import java.util.ArrayList;
+import wlvp.Attraction.AttractionDetailsView;
+import wlvp.Attraction.AttractionView;
+
+/**
+ *
+ * @author Miranda
+ */
+public class ItineraryController {
+
+    private static boolean initialized;
+    private static Itinerary itinerary;
+    private static ItineraryView itineraryView;
+    private static AttractionView attractionView; 
+    private static AttractionDetailsView attractionDetailsView; 
+    private static ArrayList<EventSlot> events;
+
+    /**
+     * Initializes the itinerary controller
+     */
+    public static void initialize() {
+        assert(!initialized);
+        
+        initialized = true;
+        itinerary = new Itinerary();
+        
+        itineraryView = new ItineraryView();
+        itineraryView.setSize(600, 600);
+
+        initialized = true;
+    }
+
+    /**
+     * Updates view to show the itinerary contents
+     */
+    public void showItinerary() {
+        events = itinerary.getEvents();
+        itineraryView.updateItineraryList(events);
+    }
+
+    /**
+     * Navigates to the Attraction view
+     */
+    public static void navigateToAttractionView() {
+        if (attractionView == null) {
+            attractionView = new AttractionView(); // Only create if not already initialized
+        }
+        attractionView.setVisible(true);
+    }
+    
+    /**
+     * Navigates to the AttractionDetails view
+     */
+    public static void navigateToAttractionDetailsView() {     
+        attractionDetailsView = new AttractionDetailsView(); 
+        attractionDetailsView.setVisible(true);
+    }
+    
+    /**
+     * Removes EventSlot from Itinerary and updates view accordingly
+     * @param eventToDelete
+     */
+    public static void removeEvent(EventSlot eventToDelete){
+        itinerary.removeEvent(eventToDelete);  
+        events = itinerary.getEvents();   
+        itineraryView.updateItineraryList(events);
+    }
+
+}
